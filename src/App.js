e926f913fbf651a9f49f8d025df6bc396e2755f4
Romeo Beaurain <beaurainr@awty.org>
2021-06-02 16:57:10 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from "./images/Logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <img className="logo" src={Logo} alt="Logo"></img>
+      <nav>
+        <ul className="navLinks">
+          <li><a href="#">Social</a></li>
+          <li><a href="#">About me</a></li>
+          <li><a href="#">Gallery</a></li>
+        </ul>
+      </nav>
+      <a href="#" className="button"><button class="buttonStyle">Contact me</button></a>
+    </header>
   );
 }
 
